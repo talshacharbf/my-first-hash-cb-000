2753875242d2d7f09b094393dbc819e66b855bed
tal shachar <tal.shachar@buzzfeed.com>
2017-12-29 09:07:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,13 +36,15 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
 
+  shipping_manifest["muskets"] = 2
+  # add 2 muskets to the shipping_manifest hash below
+shipping_manifest["gun powder"] = 4
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest
 
   # return the shipping_manifest hash below
 
