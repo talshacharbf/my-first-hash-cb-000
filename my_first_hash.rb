0ef27105bf6188e3d3eab070cc9cf039fa1ddeff
tal shachar <tal.shachar@buzzfeed.com>
2017-12-29 09:03:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,7 @@ def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
 my_hash {
-  tal => the best
+  tal => "the best"
 }
 end
 
